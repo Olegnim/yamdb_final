71d116312b35fe4b80758951ac79d9a854d7ac3c
Olegnim <olegnim@gmail.com>
2021-07-06 15:07:45 +0300

    PEP8 fix

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 
 DEBUG = False
 
-#ALLOWED_HOSTS = os.environ.get('ALLOWED_URL').split(',')
+# ALLOWED_HOSTS = os.environ.get('ALLOWED_URL').split(',')
 ALLOWED_HOSTS = ['*']
 
 
